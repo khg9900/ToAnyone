ee3942c0d56248cb75208bc005ba1d361b6afb26
MIN <90428785+minjee2758@users.noreply.github.com>
2025-04-23 18:56:40 +0900

    feat : repository interface로 변경 (#29)

--- src/main/java/com/example/toanyone/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/example/toanyone/domain/menu/repository/MenuRepository.java
@@ -1,5 +1,9 @@
 package com.example.toanyone.domain.menu.repository;
 
-public class MenuRepository {
+import com.example.toanyone.domain.menu.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Integer> {
 }
--- src/main/java/com/example/toanyone/domain/menu/service/MenuServiceImpl.java
+++ src/main/java/com/example/toanyone/domain/menu/service/MenuServiceImpl.java
@@ -1,5 +1,33 @@
 package com.example.toanyone.domain.menu.service;
 
-public class MenuServiceImpl {
+import com.example.toanyone.domain.menu.dto.MenuDto;
+import com.example.toanyone.domain.menu.entity.Menu;
+import com.example.toanyone.domain.menu.enums.MainCategory;
+import com.example.toanyone.domain.menu.enums.SubCategory;
+import com.example.toanyone.domain.menu.repository.MenuRepository;
+import com.example.toanyone.domain.store.entity.Store;
+import com.example.toanyone.domain.store.repository.StoreRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class MenuServiceImpl implements MenuService {
+
+    private final MenuRepository menuRepository;
+    private final StoreRepository storeRepository;
+
+    @Override
+    public MenuDto.Response createMenu(
+            Integer storeId, String name, String description, Integer price,
+            MainCategory mainCategory, SubCategory subCategory) {
+
+
+        Menu menu = new Menu();
+
+        return null;
+    }
 }
