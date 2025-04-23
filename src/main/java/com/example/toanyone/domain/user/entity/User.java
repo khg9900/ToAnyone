6d7441bcc6f8a3c1227ec3233bee6a4a533ab89b
khg9900 <whale4100@gmail.com>
2025-04-23 14:10:57 +0900

    feat: User Dto, User Entity, Auth Dto 생성 (#10)
    * feat: AuthController 파일 생성
    
    * feat : User Entity, User Dto, Auth Dto, Gender, UserRole 생성

--- src/main/java/com/example/toanyone/domain/user/entity/User.java
+++ src/main/java/com/example/toanyone/domain/user/entity/User.java
@@ -1,5 +1,48 @@
 package com.example.toanyone.domain.user.entity;
 
+import com.example.toanyone.domain.user.enums.Gender;
+import com.example.toanyone.domain.user.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import lombok.Getter;
+
+@Getter
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(unique = true)
+    private String nickname;
+
+    @Column(unique = true)
+    private String phone;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private LocalDate birth;
+
+    private int age;
 
 }
