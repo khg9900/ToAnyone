f1368e88f9103d62e182519a93f2e1cbbc78accf
Eunji Lee <aggee212@gmail.com>
2025-04-23 11:18:22 +0900

    feat: Store Entity 필드 설정 및 Status Enum 추가

--- src/main/java/com/example/toanyone/domain/store/entity/Store.java
+++ src/main/java/com/example/toanyone/domain/store/entity/Store.java
@@ -1,5 +1,36 @@
 package com.example.toanyone.domain.store.entity;
 
+import com.example.toanyone.domain.user.entity.User;
+import com.example.toanyone.domain.user.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name= "store")
 public class Store {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    private String name;
+    private String address;
+    private LocalDateTime openTime;
+    private LocalDateTime closeTime;
+    private Integer deliveryFee;
+    private String notice;
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    private Boolean deleted;
+    private LocalDateTime deletedAt;
+
+    //Todo BaseEntity 확인하기
 
 }
---
+++ src/main/java/com/example/toanyone/domain/user/enums/Status.java
@@ -0,0 +1,5 @@
+package com.example.toanyone.domain.user.enums;
+
+public enum Status {
+    OPEN, SOLD_OUT, TEMP_CLOSED, CLOSED;
+}
