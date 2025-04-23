52cf83136a3206b9fc433b7aa7dfc3dbf592cbcb
minjee2758 <minjee2758@naver.com>
2025-04-23 17:49:55 +0900

    feat : repository interface로 변경

--- src/main/java/com/example/toanyone/domain/cart/repository/CartRepository.java
+++ src/main/java/com/example/toanyone/domain/cart/repository/CartRepository.java
@@ -1,5 +1,9 @@
 package com.example.toanyone.domain.cart.repository;
 
-public class CartRepository {
+import com.example.toanyone.domain.cart.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Integer> {
 }
--- src/main/java/com/example/toanyone/domain/reply/repository/ReplyRepository.java
+++ src/main/java/com/example/toanyone/domain/reply/repository/ReplyRepository.java
@@ -1,5 +1,9 @@
 package com.example.toanyone.domain.reply.repository;
 
-public class ReplyRepository {
+import com.example.toanyone.domain.reply.entity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Integer> {
 }
