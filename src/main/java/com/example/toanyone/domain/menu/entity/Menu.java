1fb3e4d010bfafe97f0ed424de246bedc1f06e93
minjee2758 <minjee2758@naver.com>
2025-04-23 12:16:09 +0900

    feat/7

--- src/main/java/com/example/toanyone/domain/menu/entity/Menu.java
+++ src/main/java/com/example/toanyone/domain/menu/entity/Menu.java
@@ -1,5 +1,50 @@
 package com.example.toanyone.domain.menu.entity;
 
-public class Menu {
+import com.example.toanyone.domain.menu.enums.MainCategory;
+import com.example.toanyone.domain.menu.enums.SubCategory;
+import com.example.toanyone.domain.store.entity.Store;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "menu")
+public class Menu extends BaseMenuEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Enumerated(EnumType.STRING)
+    private MainCategory mainCategory;
+
+    @Enumerated(EnumType.STRING)
+    private SubCategory subCategory;
+
+
+
+    public Menu(Store store, String name, String description, int price ,MainCategory mainCategory, SubCategory subCategory) {
+        this.store = store;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.mainCategory = mainCategory;
+        this.subCategory = subCategory;
+    }
 
 }
