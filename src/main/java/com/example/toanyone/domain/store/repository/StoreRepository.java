9b3305d33e0de2577a61361006602735a8716146
eunji <aggee212@gmail.com>
2025-04-24 10:31:28 +0900

    Feat: Store Create (#38)
    * chor: merge 전 임시 commit
    
    * refactor: NotBlank String 제외 모두 NotNull로 변경
    
    * feat: Store Create 메서드 1차구현 (JWT 미적용)
    
    * feat: store create 구현 JWT 적용
    
    예외처리
    1. 유저 존재 여부
    2. UserRole 검증
    => 예외가 필요한지 검토 필요
    3. 가게 3개 이상 생성 금지
    4. 가게명 중복 여부

--- src/main/java/com/example/toanyone/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/toanyone/domain/store/repository/StoreRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StoreRepository extends JpaRepository<Store, Integer> {
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    int countByUserId(Long ownerId);
+
+    boolean existByName(String name);
+
 }
