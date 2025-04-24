6e2a066094e1c32e7595d665e60d5c87bcd5bddf
spko <spko0321@gmail.com>
2025-04-24 11:28:28 +0900

    orderservice 1
    # Conflicts:
    #	src/main/java/com/example/toanyone/domain/order/repository/OrderRepository.java

--- src/main/java/com/example/toanyone/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/toanyone/domain/order/repository/OrderRepository.java
@@ -2,7 +2,14 @@ package com.example.toanyone.domain.order.repository;
 
 import com.example.toanyone.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
+
+
+/*
+  주문 엔티티에 대한 기본 CRUD 기능을 제공하는 JPA 리포지토리
+  - 주문 저장, 조회 등에 사용됨
+*/
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
--- src/main/java/com/example/toanyone/domain/order/service/OrderServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.example.toanyone.domain.order.service;
-
-public class OrderServiceImpl {
-
-}
