474e26eb190736385cccf8735c8bc02323f65b6b
spko <spko0321@gmail.com>
2025-04-24 12:04:11 +0900

    orderrepository 오류 수정, mpl파일 추가 세분화3

--- src/main/java/com/example/toanyone/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/toanyone/domain/order/repository/OrderRepository.java
@@ -4,14 +4,11 @@ import com.example.toanyone.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import org.springframework.stereotype.Repository;
-
-
 /*
   주문 엔티티에 대한 기본 CRUD 기능을 제공하는 JPA 리포지토리
   - 주문 저장, 조회 등에 사용됨
 */
-
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
