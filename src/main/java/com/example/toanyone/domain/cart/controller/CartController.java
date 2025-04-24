8a345684136dc03cbcb838c8e314a9e54854a3f5
MIN <90428785+minjee2758@users.noreply.github.com>
2025-04-24 19:01:15 +0900

    Feat : 공통 응답 처리 (#63)
    * refacor : 공통 응답 추가
    
    * fix : 에러 코드 수정

--- src/main/java/com/example/toanyone/domain/cart/controller/CartController.java
+++ src/main/java/com/example/toanyone/domain/cart/controller/CartController.java
@@ -4,8 +4,11 @@ package com.example.toanyone.domain.cart.controller;
 import com.example.toanyone.domain.cart.dto.CartRequestDto;
 import com.example.toanyone.domain.cart.dto.CartResponseDto;
 import com.example.toanyone.domain.cart.service.CartService;
-import com.example.toanyone.global.common.annotation.Auth;
-import com.example.toanyone.global.common.dto.AuthUser;
+import com.example.toanyone.global.auth.annotation.Auth;
+import com.example.toanyone.global.auth.dto.AuthUser;
+import com.example.toanyone.global.common.code.SuccessStatus;
+import com.example.toanyone.global.common.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -25,14 +28,15 @@ public class CartController {
     public final CartService cartService;
 
     @PostMapping
-    public ResponseEntity<CartResponseDto> createCart(
-            @Auth AuthUser authUser, @RequestBody CartRequestDto cartRequestDto) {
+    public ResponseEntity<ApiResponse<CartResponseDto>> createCart(
+            @Auth AuthUser authUser, @Valid @RequestBody CartRequestDto cartRequestDto) {
 
         CartResponseDto cartResponseDto = cartService.createCart(
-                authUser ,cartRequestDto.getStoreId(), cartRequestDto.getMenuId(), cartRequestDto.getQuantity()
+                authUser,cartRequestDto.getStoreId(), cartRequestDto.getMenuId(), cartRequestDto.getQuantity()
         );
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(cartResponseDto);
+        return ApiResponse.onSuccess(SuccessStatus.CREATED, cartResponseDto);
+
     }
 
 }
