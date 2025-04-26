d715e00c15966410eda2dfe972423c19f27ce704
Eunji Lee <aggee212@gmail.com>
2025-04-26 17:14:01 +0900

    test: 가게 생성 최대 3개까지 가능 예외처리

--- src/test/java/com/example/toanyone/domain/store/service/CreateStoreTest.java
+++ src/test/java/com/example/toanyone/domain/store/service/CreateStoreTest.java
@@ -8,6 +8,9 @@ import com.example.toanyone.domain.user.entity.User;
 import com.example.toanyone.domain.user.enums.Gender;
 import com.example.toanyone.domain.user.enums.UserRole;
 import com.example.toanyone.domain.user.repository.UserRepository;
+import com.example.toanyone.global.auth.dto.AuthUser;
+import com.example.toanyone.global.common.code.ErrorStatus;
+import com.example.toanyone.global.common.error.ApiException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -19,6 +22,7 @@ import java.time.LocalTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
 
@@ -71,6 +75,46 @@ public class CreateStoreTest {
         assertEquals("가게가 생성되었습니다.", responseDto.getMessage());
 
     }
+
+    @Test
+    void 가게는_3개까지_만들수있다() {
+
+        Long ownerId = 1L;
+        User user = new User(
+                "eeeee@gmail.com",
+                "1234Aaa.",
+                "이름",
+                UserRole.OWNER,
+                "별명",
+                "010-1234-1234",
+                "주소",
+                Gender.FEMALE,
+                LocalDate.of(2000, 1, 1),
+                26);
+
+        StoreRequestDto.Create reqest = StoreRequestDto.Create.builder()
+                .name("가게이름")
+                .address("주소")
+                .openTime(String.valueOf(LocalTime.of(11,0,0)))
+                .closeTime(String.valueOf(LocalTime.of(21,0,0)))
+                .deliveryFee(3000)
+                .minOrderPrice(10000)
+                .notice("공지")
+                .status(String.valueOf(Status.OPEN))
+                .phone("02-123-4567")
+                .build();
+
+        //GIVEN
+        given(userRepository.findById(ownerId)).willReturn(Optional.of(user));
+        given(storeRepository.countByUserIdAndDeletedFalse(ownerId)).willReturn(3);
+
+        //WHEN
+        ApiException apiException = assertThrows(ApiException.class, () -> storeService.createStore(ownerId, reqest));
+
+        //THEN
+        assertEquals("가게는 최대 3개까지 등록 가능합니다.", apiException.getMessage());
+    }
+
 }
 
 
