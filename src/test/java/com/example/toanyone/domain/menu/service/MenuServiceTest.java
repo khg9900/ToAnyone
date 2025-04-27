c8d8b240d8f4de6153cffd364bec0223e2437f94
Eunji Lee <aggee212@gmail.com>
2025-04-27 17:53:43 +0900

    test: owner 이넘으로 받던 거 string으로 수정

--- src/test/java/com/example/toanyone/domain/menu/service/MenuServiceTest.java
+++ src/test/java/com/example/toanyone/domain/menu/service/MenuServiceTest.java
@@ -39,7 +39,7 @@ public class MenuServiceTest {
     void 가게_주인이_메뉴_생성을_정상적으로_하면_성공한다() {
         Long storeId = 1L;
         Long ownerId = 1L;
-        AuthUser authUser = new AuthUser(1L, "kkk@gmail.com", UserRole.OWNER);
+        AuthUser authUser = new AuthUser(1L, "kkk@gmail.com", "OWNER");
 
         Store store = new Store();
 
@@ -62,7 +62,7 @@ public class MenuServiceTest {
         Long ownerId = 1L;
         Long anotherOwnerId = 2L;
 
-        AuthUser authUser = new AuthUser(anotherOwnerId, "kkk@gmail.com", UserRole.OWNER);
+        AuthUser authUser = new AuthUser(anotherOwnerId, "kkk@gmail.com", "OWNER");
         Store store = new Store();
 
         given(storeRepository.findByIdOrElseThrow(storeId)).willReturn(store);
@@ -71,14 +71,14 @@ public class MenuServiceTest {
         ApiException apiException = assertThrows(ApiException.class,
                 () -> menuService.createMenu(authUser, storeId,"menu", "description", 1000, MainCategory.KOREAN, SubCategory.DRINK));
 
-        assertEquals("가게의 주인이 아니면 접근할 수 없습니다", apiException.getMessage());
+        assertEquals("가게의 주인이 아니면 접근할 수 없습니다.", apiException.getMessage());
     }
 
     @Test
     void 이미_있는_메뉴는_추가할_수_없다(){
         Long storeId = 1L;
         Long ownerId = 1L;
-        AuthUser authUser = new AuthUser(1L, "kkk@gmail.com", UserRole.OWNER);
+        AuthUser authUser = new AuthUser(1L, "kkk@gmail.com", "OWNER");
         Store store = new Store();
 
         //given
